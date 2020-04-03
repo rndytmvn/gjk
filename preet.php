adb0d4b25e553231fb39bfe16f38459b0b5a0c9e
rndytmvn <63040947+rndytmvn@users.noreply.github.com>
2020-04-03 09:09:20 +0700

    Update bang preet.php

--- preet.php
+++ preet.php
@@ -104,7 +104,7 @@ echo color("green"," =================================== \n");
 						sleep(1);
 					}
 					sleep(3);
-					$goride1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"COBAGOFOOD090320A"}');
+					$goride1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"COBAGOFOOD010420A"}');
 					$message2 = fetch_value($goride1,'"message":"','"');
 					echo "\n".color("green","+] Message: ".$message2);
 					sleep(3);
